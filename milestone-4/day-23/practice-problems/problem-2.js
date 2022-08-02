25d9c5312cc29076f0e93cd04946e9552c4ff453
azharanowar <azharanowar@gmail.com>
2022-08-03 00:49:31 +0600

    Problem 2 solved

---
+++ milestone-4/day-23/practice-problems/problem-2.js
@@ -0,0 +1,11 @@
+var mathMarks = 75.25;
+var bioloryMarks = 65;
+var chemistryMarks = 80;
+var physicsMarks = 35.45;
+var banglaMarks = 99.50;
+
+
+avarageMarks = (mathMarks + bioloryMarks + chemistryMarks + physicsMarks + banglaMarks) / 5;
+avarageMarks = avarageMarks.toFixed(2);
+
+console.log(parseFloat(avarageMarks));
