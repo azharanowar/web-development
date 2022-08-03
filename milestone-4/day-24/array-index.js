8cfaab48c9b40260d2fee6b55ba3cd4aca923969
azharanowar <azharanowar@gmail.com>
2022-08-03 14:34:01 +0600

    Javascript push() and  pop() practiced

---
+++ milestone-4/day-24/array-index.js
@@ -0,0 +1,25 @@
+var number = [ 10, 20, 30, 50, 100, 200 ];
+var studentsName = [ "Azhar", "Anowar", "Arafat", "Ali", "Nihad" ];
+
+
+// console.log(number);
+// console.log(studentsName);
+
+
+// Find index number from a array elements value
+
+// console.log(studentsName.indexOf("Arafat")); // output: 2
+
+
+// Push is used to add a new element to an array
+console.log( number );
+number.push( 500 );
+console.log( number );
+
+
+// Pop is used to remove an elemets from array
+console.log( studentsName );
+studentsName.pop();
+console.log( studentsName );
+
+
