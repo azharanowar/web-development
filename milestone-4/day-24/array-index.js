2d3aa17d825e2a8371a29174f0424ebb8b1b3417
azharanowar <azharanowar@gmail.com>
2022-08-03 16:58:30 +0600

    Adding elements to an array any particular place by index with javascript splice()

--- milestone-4/day-24/array-index.js
+++ milestone-4/day-24/array-index.js
@@ -1,4 +1,4 @@
-var number = [ 10, 20, 30, 50, 100, 200 ];
+var numbers = [ 10, 20, 30, 50, 100, 200 ];
 var studentsName = [ "Azhar", "Anowar", "Arafat", "Ali", "Nihad" ];
 
 
@@ -7,19 +7,22 @@ var studentsName = [ "Azhar", "Anowar", "Arafat", "Ali", "Nihad" ];
 
 
 // Find index number from a array elements value
-
 // console.log(studentsName.indexOf("Arafat")); // output: 2
 
 
 // Push is used to add a new element to an array
-console.log( number );
-number.push( 500 );
-console.log( number );
+// console.log( numbers );
+// numbers.push( 500 );
+// console.log( numbers );
 
 
 // Pop is used to remove an elemets from array
-console.log( studentsName );
-studentsName.pop();
-console.log( studentsName );
+// console.log( studentsName );
+// studentsName.pop();
+// console.log( studentsName );
 
 
+// Add a new element to an array particular index
+console.log(numbers);
+numbers.splice(0, 0, 20); // here first arguments is index number, second one is how many elements to remove and third one is value to add.
+console.log(numbers);
