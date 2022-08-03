6a240f7acc82820bf71840497b87b9c8c32c63c5
azharanowar <azharanowar@gmail.com>
2022-08-03 12:58:07 +0600

    Javascript array declaration and print value by inxed number

---
+++ milestone-4/day-24/array.js
@@ -0,0 +1,15 @@
+var numbers = [ 10, 20, 30, 50, 100 ];
+var studentSRoll = [181040, 169721, 169748, 169756, 169775];
+var studentsName = ["Anowar", "Fozle", "Nihad", "Shorip", "Asif"];
+
+
+// Print array to console
+console.log(numbers);
+
+
+// Print a value of array by index number
+console.log(numbers[2]);
+
+console.log(numbers[0]);
+
+
