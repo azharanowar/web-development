384f42299d7906def4b61e46a5f2309b36798160
azharanowar <azharanowar@gmail.com>
2022-08-04 09:14:11 +0600

    Practice problem-3 second part finding isosceles triengle or not program solved

--- milestone-4/day-24/practice-problems/problem-3.js
+++ milestone-4/day-24/practice-problems/problem-3.js
@@ -1,3 +1,5 @@
+// Finding largest number program from three given value;
+
 var numberOne = 183;
 var numberTwo = 79;
 var numberThree = 145;
@@ -8,4 +10,18 @@ if ( numberOne > numberTwo && numberOne > numberThree ) {
     console.log( "Largest number is number two " + numberTwo );
 } else {
     console.log( "Largest number is number three " + numberThree );
+}
+
+
+// Isosceles triangle program with if else
+var triengleSideOne = 2;
+var triengleSideTwo = 2;
+var triengleSideThree = 1;
+
+if ( (triengleSideOne === triengleSideTwo) || (triengleSideOne === triengleSideThree) ) {
+    console.log( "The triengle is isosceles, means two sides are equal." );
+} else if ( (triengleSideTwo === triengleSideOne) || (triengleSideTwo === triengleSideThree) ) {
+    console.log( "The triengle is isosceles, means two sides are equal." );
+} else {
+    console.log( "The triengle is not isosceles." );
 }
