b2e857ca6855fbb0e495d1b8fb9b1275fdc4a01e
azharanowar <azharanowar@gmail.com>
2022-08-04 23:46:04 +0600

    Another two more practice completed

--- milestone-4/day-26/practice.js
+++ milestone-4/day-26/practice.js
@@ -1,13 +1,28 @@
 // Print "আসকে আমার মন ভালো নেই" 39 times.
 
-for ( i = 1; i <= 39; i++ ){
-    console.log("আসকে আমার মন ভালো নেই");
-}
+// for ( i = 1; i <= 39; i++ ){
+//     console.log("আসকে আমার মন ভালো নেই");
+// }
 
 
 // Print 58 to 98 all numbers.
-num = 58;
-while ( num <= 98 ) {
-    console.log(num);
-    num++;
+
+// num = 58;
+// while ( num <= 98 ) {
+//     console.log(num);
+//     num++;
+// }
+
+
+// Print all even numbers from 412 to 456.
+
+// for ( i = 412; i <= 456; i += 2 ) {
+//     console.log(i);
+// }
+
+
+
+var learnedTillNow = [ "Variable", "Array", "indexOf()", "parseInt()", "parseFloat()", "toFixed()", "array.length", "break", "continue", "Bug Fixing" ];
+for ( i = 0; i < learnedTillNow.length; i++ ) {
+    console.log(learnedTillNow[i]);
 }
