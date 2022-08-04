282031675569489ad3cc8e59daf29134c427f880
azharanowar <azharanowar@gmail.com>
2022-08-04 09:01:24 +0600

    Practice problem-3 first part of finding largest number program solved

---
+++ milestone-4/day-24/practice-problems/problem-3.js
@@ -0,0 +1,11 @@
+var numberOne = 183;
+var numberTwo = 79;
+var numberThree = 145;
+
+if ( numberOne > numberTwo && numberOne > numberThree ) {
+    console.log( "Largest number is number one: " + numberOne );
+} else if ( numberTwo > numberOne && numberTwo > numberThree ) {
+    console.log( "Largest number is number two " + numberTwo );
+} else {
+    console.log( "Largest number is number three " + numberThree );
+}
