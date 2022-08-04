0b287f5257c6a893326d2b31de321d788258ee8a
azharanowar <azharanowar@gmail.com>
2022-08-04 09:50:30 +0600

    Day-5 while-loop practiced

---
+++ milestone-4/day-25/while-loop.js
@@ -0,0 +1,7 @@
+roastGiven = 0;
+while ( roastGiven <= 5 ) {
+    console.log("Roast Den Akhane Please!");
+    console.log("Aii Lon Dilam!");
+    console.log("Koibar Deya Hoice: " + roastGiven );
+    roastGiven++
+}
