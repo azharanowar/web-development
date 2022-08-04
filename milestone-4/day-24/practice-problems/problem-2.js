9b6246e6a7e3c445743b7c43fce9b45146db76eb
azharanowar <azharanowar@gmail.com>
2022-08-04 08:47:33 +0600

    Practice problem-2 solved

---
+++ milestone-4/day-24/practice-problems/problem-2.js
@@ -0,0 +1,13 @@
+score = 85;
+
+if ( score >= 80 ) {
+    console.log("You have passed and your grad point is A");
+} else if ( score >= 60 ) {
+    console.log("You have passed and your grad point is B");
+} else if ( score >= 50 ) {
+    console.log("You have passed and your grad point is C");
+} else if ( score >= 40 ) {
+    console.log("You have passed and your grad point is D");
+} else if ( score <= 39 ) {
+    console.log("Sorry, You have failed try again next time.");
+}
