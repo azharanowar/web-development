e162bd61d3f77735d73fafe1bc40d7a139908169
azharanowar <azharanowar@gmail.com>
2022-08-04 23:34:10 +0600

    Two practice program completed

---
+++ milestone-4/day-26/practice.js
@@ -0,0 +1,13 @@
+// Print "আসকে আমার মন ভালো নেই" 39 times.
+
+for ( i = 1; i <= 39; i++ ){
+    console.log("আসকে আমার মন ভালো নেই");
+}
+
+
+// Print 58 to 98 all numbers.
+num = 58;
+while ( num <= 98 ) {
+    console.log(num);
+    num++;
+}
