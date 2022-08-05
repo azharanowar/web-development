6d1ba2cde1dbf33cda942a34f851a2897595a8ec
azharanowar <azharanowar@gmail.com>
2022-08-05 09:47:29 +0600

    Javascript adding new elements to an array index with splice()

--- milestone-4/day-27/recap.js
+++ milestone-4/day-27/recap.js
@@ -1,5 +1,9 @@
 var numbers = [ 20, 30, 12, 40, 130, 40, 50, 120, 5, 32 ];
 
+// Adding a new elements to an array with index position.
+// numbers.splice( 0, 0, 10 );
+// console.log(numbers);
+
 // slice() 
 // console.log(numbers.slice( 2, 5 ));
 
