0e227f5ea68f5c259d50de3ae42e5caf80f3b1be
azharanowar <azharanowar@gmail.com>
2022-08-05 09:59:06 +0600

    javascript function declaration and calling

---
+++ milestone-4/day-27/function.js
@@ -0,0 +1,20 @@
+/*
+    function functionName(perameters) {
+        function body...
+    }
+
+    //Function Calling: 
+    functionName(arguments);
+*/
+
+
+// //Function declaration:
+// function stratFan() {
+//     console.log("Stand Up!");
+//     console.log("GO Towards the fan switch");
+//     console.log("Press The Fan Switch!");
+// }
+
+
+// //Function calling:
+// stratFan();
