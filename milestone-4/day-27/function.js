653079de37e0743ce9dd3d8c732ee5a08a1479be
azharanowar <azharanowar@gmail.com>
2022-08-05 10:04:11 +0600

    sum() function decalation with two perameters and calling multiple times with two arguments

--- milestone-4/day-27/function.js
+++ milestone-4/day-27/function.js
@@ -18,3 +18,17 @@
 
 // //Function calling:
 // stratFan();
+
+
+// sum() function declaration with two perameters:
+function sum( firstNumber, secondNumber ) {
+    console.log( firstNumber + secondNumber );
+}
+
+// sum() function calling multiple times with two arguments:
+sum( 100, 100 );
+sum( 100, 50 );
+sum( 120, 500 );
+sum( 50, 50 );
+sum( 10, 20 );
+sum( 100, 30 );
