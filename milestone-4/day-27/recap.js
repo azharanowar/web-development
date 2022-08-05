ea6f32e7f1b33ec132c39a6f8cd2c6603f3fa79c
azharanowar <azharanowar@gmail.com>
2022-08-05 09:51:40 +0600

    includes() comment text change

--- milestone-4/day-27/recap.js
+++ milestone-4/day-27/recap.js
@@ -8,6 +8,6 @@ var numbers = [ 20, 30, 12, 40, 130, 40, 50, 120, 5, 32 ];
 // console.log(numbers.slice( 2, 5 ));
 
 
-// Find something from a string
-var collegeName = "Feni Polytechnic Institute";
-console.log(collegeName.includes("Feni"));
+// Find something is in the string or not
+// var collegeName = "Feni Polytechnic Institute";
+// console.log(collegeName.includes("Feni"));
