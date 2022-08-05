52d65ad5781295ef22a2588c3f62d9371343f545
azharanowar <azharanowar@gmail.com>
2022-08-05 09:32:28 +0600

    javascript includes() practiced

--- milestone-4/day-27/recap.js
+++ milestone-4/day-27/recap.js
@@ -1,4 +1,9 @@
 var numbers = [ 20, 30, 12, 40, 130, 40, 50, 120, 5, 32 ];
 
 // slice() 
-console.log(numbers.slice( 2, 5 ));
+// console.log(numbers.slice( 2, 5 ));
+
+
+// Find something from a string
+var collegeName = "Feni Polytechnic Institute";
+console.log(collegeName.includes("Feni"));
