0a268ae40768a808cc31f446e6c154e1a70807e1
azharanowar <azharanowar@gmail.com>
2022-08-06 11:22:20 +0600

    Get object value way-1 by (.) dot object sign with keys or properties name

--- milestone-4/day-27/properties.js
+++ milestone-4/day-27/properties.js
@@ -7,7 +7,7 @@ var studentInfo = {
 }
 
 
-// Get object value keys
+// Get object value way-1 by (.) object sign with keys or properties name
 console.log(studentInfo.name);
 console.log(studentInfo.roll);
 
