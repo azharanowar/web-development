6ac17307ccd8a47c5e0a58bbca81d2cd34d2eb7e
azharanowar <azharanowar@gmail.com>
2022-08-06 11:26:38 +0600

    Get object value way-2 by array sign [] with keys or peroperties name.

--- milestone-4/day-27/properties.js
+++ milestone-4/day-27/properties.js
@@ -7,7 +7,12 @@ var studentInfo = {
 }
 
 
-// Get object value way-1 by (.) object sign with keys or properties name
+// Get object value way-1 by (.) dot object sign with keys or properties name.
 console.log(studentInfo.name);
 console.log(studentInfo.roll);
+
+// Get object value way-2 by array sign [] with keys or peroperties name.
+console.log(studentInfo['depertment']);
+var group = 'group';
+console.log(studentInfo[group]);
 
