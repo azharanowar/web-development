5bb552015f2a2ea4afbd0f2aab1271de027749b2
azharanowar <azharanowar@gmail.com>
2022-08-06 11:06:55 +0600

    Even odd number check funtion with perameter and return

--- milestone-4/day-27/practice/practice-4.js
+++ milestone-4/day-27/practice/practice-4.js
@@ -11,3 +11,18 @@ evenOddChecker(12);
 evenOddChecker(16);
 evenOddChecker(21);
 evenOddChecker(111);
+
+
+
+function evenOddNumberChecker( number ) {
+    var result;
+    if ( number % 2 == 0 ) {
+        result = number + " is a even number";
+    } else {
+        result = number + " is a odd number";
+    }
+    return result;
+}
+
+var result = evenOddNumberChecker(20);
+console.log(result);
