50efed9f66d4d6e419f0cb68bdbcefb9c5ca96d1
azharanowar <azharanowar@gmail.com>
2022-08-06 12:40:26 +0600

    Random signals change function

--- milestone-4/day-27/practice/practice-5.js
+++ milestone-4/day-27/practice/practice-5.js
@@ -1,19 +1,38 @@
-function trafficSignals(signal) {
+// function trafficSignal(signal) {
+//     var result;
+//     if ( signal == "red" ) {
+//         result = "Don't cross the road now, It will be DANGER!!!";
+//     } else if ( signal == "yellow" ) {
+//         result = "Better you should stop now!!! Later you can cross the road.";
+//     } else {
+//         result = "Now you can safely cross the road. Have a great day.";
+//     }
+
+//     return result;
+// }
+
+
+// var result = trafficSignal("green");
+// console.log(result);
+
+
+
+
+// Random signals change function:
+function trafficSignals(singals) {
+    var signal = Math.floor( Math.random() * singals.length );
     var result;
-    if ( signal == "red" ) {
+    if ( signal == 0 ) {
         result = "Don't cross the road now, It will be DANGER!!!";
-    } else if ( signal == "yellow" ) {
-        result = "Better you should stop now!!! Later you can cross the road."
-    } else {
-        result = "Now you can safely cross the road. Have a great day."
+    } else if ( signal == 1 ) {
+        result = "Better you should stop now!!! Later you can cross the road.";
+    } else if ( signal == 2) {
+        result = "Now you can safely cross the road. Have a great day.";
     }
 
     return result;
 }
 
-
-var result = trafficSignals("green");
+var singals = [ "red", "yellow", "green" ];
+var result = trafficSignals(singals);
 console.log(result);
-
-
-
