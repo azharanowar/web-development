8838474bd93cb94d4f3c27eb5454971f91535bc2
azharanowar <azharanowar@gmail.com>
2022-08-06 10:51:16 +0600

    Even odd number checking function that has perameter but no return

---
+++ milestone-4/day-27/practice/practice-4.js
@@ -0,0 +1,13 @@
+function evenOddChecker( number ) {
+    if ( number % 2 == 0 ) {
+        console.log(number, "is a even number.");
+    } else {
+        console.log(number, "is a odd number");
+    }
+}
+
+evenOddChecker(5);
+evenOddChecker(12);
+evenOddChecker(16);
+evenOddChecker(21);
+evenOddChecker(111);
