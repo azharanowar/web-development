6eb9418bace69d24ef624f0199f9fb41928c1bc6
azharanowar <azharanowar@gmail.com>
2022-08-06 11:33:50 +0600

    Get object all properties/keys name by object.keys()

--- milestone-4/day-27/properties.js
+++ milestone-4/day-27/properties.js
@@ -7,14 +7,18 @@ var studentInfo = {
 }
 
 
-// Get object value way-1 by (.) dot object sign with keys or properties name.
+// Get object value way-1 by (.) dot object sign with keys or properties name:
 console.log(studentInfo.name);
 console.log(studentInfo.roll);
 var studentName = studentInfo.name;
 console.log(studentName);
 
-// Get object value way-2 by array sign [] with keys or peroperties name.
+// Get object value way-2 by array sign [] with keys or peroperties name:
 console.log(studentInfo['depertment']);
 var group = 'group';
 console.log(studentInfo[group]);
 
+
+// Get object all properties/keys name by Object.keys():
+var studentInfoKeys = Object.keys(studentInfo);
+console.log(studentInfoKeys);
