a9c013fbf24923979dcfc0acf52bc060fdfee860
azharanowar <azharanowar@gmail.com>
2022-08-06 11:28:42 +0600

    Get object value way-1 by variable

--- milestone-4/day-27/properties.js
+++ milestone-4/day-27/properties.js
@@ -10,6 +10,8 @@ var studentInfo = {
 // Get object value way-1 by (.) dot object sign with keys or properties name.
 console.log(studentInfo.name);
 console.log(studentInfo.roll);
+var studentName = studentInfo.name;
+console.log(studentName);
 
 // Get object value way-2 by array sign [] with keys or peroperties name.
 console.log(studentInfo['depertment']);
