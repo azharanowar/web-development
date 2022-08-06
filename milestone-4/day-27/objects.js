3fd8ead001a6247429f7e4f5b4025e26c6ff4261
azharanowar <azharanowar@gmail.com>
2022-08-06 10:17:20 +0600

    Javascript objects in array and printing all values

--- milestone-4/day-27/objects.js
+++ milestone-4/day-27/objects.js
@@ -17,3 +17,60 @@ let mobile = {
 
 console.log( student );
 console.log( student.name );
+
+
+
+// Array with objects
+let students = [
+    {
+        roll: 181040,
+        name: "Azhar Anowar",
+        depertment: "Computer",
+        shift: "2nd",
+        group: "B"
+    },
+    {
+        roll: 181040,
+        name: "Fozle Arafat",
+        depertment: "Computer",
+        shift: "2nd",
+        group: "A"
+    },
+    {
+        roll: 181040,
+        name: "Ariful Islam",
+        depertment: "Computer",
+        shift: "2nd",
+        group: "A"
+    },
+    {
+        roll: 181040,
+        name: "Shakhawat Hossen",
+        depertment: "Computer",
+        shift: "2nd",
+        group: "B"
+    },
+    {
+        roll: 181040,
+        name: "Mohammad Ali",
+        depertment: "Computer",
+        shift: "2nd",
+        group: "A"
+    },
+];
+
+// console.log(students[1].name);
+
+for ( i = 0; i < students.length; i++) {
+    var roll        = students[i].roll;
+    var name        = students[i].name;
+    var depertment  = students[i].depertment;
+    var shift       = students[i].shift;
+    var group       = students[i].group;
+
+    console.log( "Student name is: ", name );
+    console.log( "Roll number is: ", roll );
+    console.log( "Depertment: ", depertment );
+    console.log( "Shift: ", shift );
+    console.log( "Group: ", group, "\n" );
+}
