128d31a1a3691e7ea19132b1e2b312718e1f96ba
azharanowar <azharanowar@gmail.com>
2022-08-08 19:33:28 +0600

    Difference of var, let, const

---
+++ milestone-4/day-28/var-let-const.js
@@ -0,0 +1,13 @@
+var age1 = 22;
+var age1 = 40;
+
+
+let age2 = 25;
+// let age2 = 32; // can't declare let again.
+age2 = 30; // But we can update value
+
+const age3 = 30;
+// age3 = 40; // We can't change const value
+
+
+console.log(age1, age2, age3);
