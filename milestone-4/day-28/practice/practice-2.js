4c7683b4f7de917387d6e576ff08277acb6204ae
azharanowar <azharanowar@gmail.com>
2022-08-08 22:51:00 +0600

    Leap year function return an array

--- milestone-4/day-28/practice/practice-2.js
+++ milestone-4/day-28/practice/practice-2.js
@@ -1,11 +1,15 @@
 function findLeapYears( years ) {
+    let leapYears = [];
     for ( var year of years ) {
         if ( (year % 4 == 0) && (year % 100 !== 0) || (year % 400 == 0) ) {
-            console.log( "This is a leap year", year );
+            leapYears.push(year);
         }
     }
+
+    return leapYears;
 }
 
 
 let years = [2012, 2023,2024,2025,2028,2030];
-findLeapYears( years );
+let leapYears = findLeapYears( years );
+console.log(leapYears)
