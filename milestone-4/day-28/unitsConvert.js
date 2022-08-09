183bfdcd1eba80a261caeeb57990814c8f07eab2
azharanowar <azharanowar@gmail.com>
2022-08-09 11:52:46 +0600

    Mile to Kilometer JS Program

--- milestone-4/day-28/unitsConvert.js
+++ milestone-4/day-28/unitsConvert.js
@@ -9,3 +9,18 @@ const result1 = incheToFeet( 24 );
 const result2 = incheToFeet( 48 );
 const result3 = incheToFeet( 16 );
 console.log(result1, result2, result3);
+
+
+
+
+// Mile to Kilometer Converter
+
+function mileToKilometer( mile ) {
+    const kilometer = mile * 1.609;
+    return kilometer;
+}
+
+const kiloResult1 = mileToKilometer( 100 );
+const kiloResult2 = mileToKilometer( 43 );
+const kiloResult3 = mileToKilometer( 50 );
+console.log( kiloResult1, kiloResult2, kiloResult3 );
