904dfc7cb84012c3480e2664a9fa718c3ac41d43
azharanowar <azharanowar@gmail.com>
2022-08-09 17:17:25 +0600

    Area of rectangle by length and width

---
+++ milestone-4/day-30/practice/areaOfRectangle.js
@@ -0,0 +1,14 @@
+// Area of rectangle by width and length program 
+
+function areaOfRectangle ( length, width ) {
+    const result = length * width;
+
+    return result;
+}
+
+
+const result1 = areaOfRectangle( 10, 10 );
+const result2 = areaOfRectangle( 100, 50 );
+const result3 = areaOfRectangle( 50, 20 );
+
+console.log( result1, result2, result3 );
