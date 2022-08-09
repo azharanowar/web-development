adf9dbe2c26305b25f11b865e716c7e36c540a05
azharanowar <azharanowar@gmail.com>
2022-08-09 12:34:42 +0600

    Factorial by while loop or a decrement loop

--- milestone-4/day-28/factorial.js
+++ milestone-4/day-28/factorial.js
@@ -13,3 +13,22 @@ const result2 = factorial( 5 );
 const result3 = factorial( 6 );
 
 console.log( result1, result2, result3 );
+
+
+
+
+
+// Factorial using while loop
+
+function factorialByWhile( number ) {
+    let fact = 1;
+    while ( number >= 1 ) {
+        fact *= number;
+        number--;
+    }
+
+    return fact;
+}
+
+const res = factorialByWhile( 5 );
+console.log( res );
