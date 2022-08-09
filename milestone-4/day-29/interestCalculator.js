8388d92bad69f5128fdba773a71107e45b52b0ae
azharanowar <azharanowar@gmail.com>
2022-08-09 14:41:56 +0600

    Compound / Complex Interest calculator in JS

--- milestone-4/day-29/interestCalculator.js
+++ milestone-4/day-29/interestCalculator.js
@@ -12,3 +12,28 @@ function simpleInterest ( p, r, t ) {
 
 const finalAmount = simpleInterest( 100, 10, 2 );
 console.log( finalAmount );
+
+
+
+
+
+// Compound / Complex Interest Calculator. Using the formula A = P(1 + r/n)nt   ( Principal (P), Annual Rate (R): %, Compound (n): Annually, Time (t in years) )
+
+function compoundInterest( principalBalance, annualRate, compound, time) {
+
+    const p  = principalBalance;
+    const r  = annualRate / 100;
+    const n  = compound;
+    const nt = time;
+
+    const finalAmount = p * Math.pow( ( 1 + r / n ), nt );
+
+
+    return finalAmount;
+}
+
+const compoundInterestFinalAmount = compoundInterest( 1000, 10, 1, 2 );
+console.log( compoundInterestFinalAmount );
+
+const compoundInterestFinalAmount2 = compoundInterest( 150, 20, 1, 5 );
+console.log( compoundInterestFinalAmount2 );
