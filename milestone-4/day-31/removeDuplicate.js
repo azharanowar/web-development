67f369962a2dadb8d847236af8101e9bfdc103e0
azharanowar <azharanowar@gmail.com>
2022-08-10 21:41:07 +0600

    I'll never give up with removing duplicate value from an array without using a new array and js available functions

---
+++ milestone-4/day-31/removeDuplicate.js
@@ -0,0 +1,38 @@
+
+
+
+
+
+
+
+
+
+
+
+// function removeDuplicateValues( studentsName ) {
+//     let count = 0;
+//     for ( i = 0; i < studentsName.length; i++ ) {
+//         for ( j = 0; j < i; j++ ) {
+//             if ( studentsName[i] === studentsName[j] ) {
+//                 studentsName[i] = undefined;
+//                 [ studentsName[j], studentsName[j+1] ] = [ studentsName[j+1], studentsName[j] ];
+//             }
+//         }
+
+//         if ( studentsName[i] == undefined ) {
+//             count++;
+//         }
+//     }
+
+//     console.log(studentsName.length);
+//     console.log(count);
+//     console.log(studentsName);
+
+//     // studentsName.length -= count;
+//     // return studentsName;
+// }
+
+
+// const studentsName = [ "Ali", "Arafat", "Shorip", "Anowar", "Nihad", "Omar", "Arafat", "Said", "Shorip", "Anowar", "Shakhawat", "Shakhawat", "Omar", "Shakhawat", "Omar", "Omar", "Ali" ];
+// const uniqueStudentsName = removeDuplicateValues( studentsName );
+// console.log( uniqueStudentsName );
