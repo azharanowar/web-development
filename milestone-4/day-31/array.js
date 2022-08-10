f0ac1146fe0a9f7d4933c9f3596113a146cdfc07
azharanowar <azharanowar@gmail.com>
2022-08-10 09:58:54 +0600

    Check array or not in JS Array.isArray()

--- milestone-4/day-31/array.js
+++ milestone-4/day-31/array.js
@@ -1,5 +1,29 @@
 let name = "Azhar Anowar";
 const age = 20;
+const students = {
+    name: "Mohammad Ali",
+    age: 21,
+    roll: 169721
+}
 
+function sum( sum1, sum2 ) {
+    const result =  sum1 + sum2;
+    return result;
+}
+
+sum();
+
+
+const rolls = [ 169756, 169775, 181040 ];
+
+
+
+console.log( typeof name );
 console.log( typeof age );
-console.log(  );
+console.log( typeof students );
+console.log( typeof sum );
+console.log( typeof rolls ); // To check is that array or not typeof is not works. It say array as a object.
+
+console.log( Array.isArray( students ) );
+console.log( Array.isArray( sum ) );
+console.log( Array.isArray( rolls ) );
