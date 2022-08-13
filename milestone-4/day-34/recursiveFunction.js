5c7a8974fce8d111d76714fd5d38fa036fce97b5
azharanowar <azharanowar@gmail.com>
2022-08-13 10:27:07 +0600

    Sum Series by Recursive Function

--- milestone-4/day-34/recursiveFunction.js
+++ milestone-4/day-34/recursiveFunction.js
@@ -1,13 +1,32 @@
 // Sum series by for loop
 
-function sumSeries( n ) {
-    let sum = 0;
-    for ( i = 1; i <= n; i++ ) {
-        sum += i;
-    }
+// function sumSeries( n ) {
+//     let sum = 0;
+//     for ( i = 1; i <= n; i++ ) {
+//         sum += i;
+//     }
 
-    return sum;
+//     return sum;
+// }
+
+// const sum = sumSeries( 5 );
+// console.log( sum );
+
+
+
+
+
+// Sum Sereis by Recursive Function
+
+let sum = 0;
+function sumSeries( n ) {
+    sum += n;
+    if ( n === 1 ) {
+        return 1;
+    }
+    return sumSeries( n - 1 );
 }
 
-const sum = sumSeries( 5 );
+sumSeries( 5 );
 console.log( sum );
+
