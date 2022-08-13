d4580520f4202780678ca60958cbfb8de221d1ab
azharanowar <azharanowar@gmail.com>
2022-08-13 20:44:38 +0600

    JavaScript document.getElementsByTagName() practice

--- milestone-5/day-35/dom.js
+++ milestone-5/day-35/dom.js
@@ -1,1 +1,10 @@
 console.log( "Hello World!" );
+console.log(document)
+
+let listItems = document.getElementsByTagName('li');
+
+console.log(listItems);
+
+for ( let item of listItems ) {
+    console.log( item );
+}
