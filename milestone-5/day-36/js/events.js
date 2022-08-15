49b665bdd07667ac44be0517d5af6be809faee09
azharanowar <azharanowar@gmail.com>
2022-08-15 12:45:46 +0600

    Recommended Way with calling id and the set an Anonymous Function

--- milestone-5/day-36/js/events.js
+++ milestone-5/day-36/js/events.js
@@ -10,3 +10,9 @@ makeBlueBtn.onclick = makeBlue;
 function makeBlue() {
     document.body.style.backgroundColor = 'blue';
 }
+
+
+const makeGreenBtn = document.getElementById("makeGreen");
+makeGreenBtn.onclick = function() {
+    document.body.style.backgroundColor = 'green';
+}
