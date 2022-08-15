1512d46973f76844769429baaa8d403505d3c2cd
azharanowar <azharanowar@gmail.com>
2022-08-15 14:35:55 +0600

    Comment added to three options

--- milestone-5/day-36/js/events.js
+++ milestone-5/day-36/js/events.js
@@ -1,8 +1,9 @@
+// JS event calling option 1:
 function makeRed() {
     document.body.style.backgroundColor = 'red';
 }
 
-
+// JS event calling option 2:
 const makeBlueBtn = document.getElementById("makeBlue");
 makeBlueBtn.onclick = makeBlue;
 
@@ -11,7 +12,7 @@ function makeBlue() {
     document.body.style.backgroundColor = 'blue';
 }
 
-
+// JS event calling option 3:
 const makeGreenBtn = document.getElementById("makeGreen");
 makeGreenBtn.onclick = function() {
     document.body.style.backgroundColor = 'green';
