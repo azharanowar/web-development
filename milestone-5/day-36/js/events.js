be02e83811e5b8ec58ecfdd2b02dc8bf73acfe27
azharanowar <azharanowar@gmail.com>
2022-08-15 18:11:37 +0600

    Comment correction

--- milestone-5/day-36/js/events.js
+++ milestone-5/day-36/js/events.js
@@ -4,7 +4,7 @@
 
 
 
-// JS event calling option 2 (Call function in html event attributes) we will use this way sometimes:
+// JS event calling option 2 (Call function in html event attributes) we will use this sometimes:
 function makeRed() {
     document.body.style.backgroundColor = 'red';
 }
@@ -12,7 +12,7 @@ function makeRed() {
 
 
 
-// JS event calling option 3 (selecting element by id and function calling) we will use this way many times and this is a standard way:
+// JS event calling option 3 (selecting element by id and function calling) we will use this often and this is a standard way:
 const makeBlueBtn = document.getElementById("makeBlue");
 makeBlueBtn.onclick = makeBlue;
 
@@ -23,7 +23,7 @@ function makeBlue() {
 
 
 
-// JS event calling option 3 another (selecting element by id and use a anonymous function) we will use this way many times and this is a standard way:
+// JS event calling option 3 another (selecting element by id and use a anonymous function) we will use this sometimes and this is a standard way:
 const makeGreenBtn = document.getElementById("makeGreen");
 makeGreenBtn.onclick = function() {
     document.body.style.backgroundColor = 'green';
@@ -32,7 +32,7 @@ makeGreenBtn.onclick = function() {
 
 
 
-// JS event calling option 4 (addEventListener with function calling) we will use this way many times and this is a standard way:
+// JS event calling option 4 (addEventListener with function calling) we will use this often and this is a standard way:
 const makePinkBtn = document.getElementById('makePink');
 makePinkBtn.addEventListener('click', makePink);
 
@@ -43,7 +43,7 @@ function makePink() {
 
 
 
-// JS event calling option 4 another (addEventListener with a anonymous function) we will use this way many times and this is a standard way:
+// JS event calling option 4 another (addEventListener with a anonymous function) we will use this sometimes and this is a standard way:
 const makeOrangeBtn = document.getElementById('makeOrange');
 makeOrangeBtn.addEventListener('click', function () {
     document.body.style.backgroundColor = 'orange';
