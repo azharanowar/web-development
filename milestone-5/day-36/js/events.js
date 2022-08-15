72feace954b64a52404427f0fa8f251d58ce068d
azharanowar <azharanowar@gmail.com>
2022-08-15 12:43:10 +0600

    Recommended Way One with Calling ID then set Function

--- milestone-5/day-36/js/events.js
+++ milestone-5/day-36/js/events.js
@@ -1,3 +1,12 @@
 function makeRed() {
     document.body.style.backgroundColor = 'red';
 }
+
+
+const makeBlueBtn = document.getElementById("makeBlue");
+makeBlueBtn.onclick = makeBlue;
+
+
+function makeBlue() {
+    document.body.style.backgroundColor = 'blue';
+}
