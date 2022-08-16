6f01932efae0941199c5843c03a2a92eb1062175
azharanowar <azharanowar@gmail.com>
2022-08-16 21:34:48 +0600

    Focus Event Triggered

---
+++ milestone-5/day-36/js/moreEvent.js
@@ -0,0 +1,7 @@
+// document.getElementById("eventBtn").addEventListener('click', function() {
+//     console.log("On Click Event");
+// });
+
+document.getElementById("inputField").addEventListener('focus', function() {
+    console.log("Focus Event Triggered!")
+});
