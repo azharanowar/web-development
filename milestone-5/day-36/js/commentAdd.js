afc7af16bae76e6d8849f1df22c2e25f8862ca7a
azharanowar <azharanowar@gmail.com>
2022-08-16 18:59:48 +0600

    Comment add few changes with steps comment added

--- milestone-5/day-36/js/commentAdd.js
+++ milestone-5/day-36/js/commentAdd.js
@@ -1,9 +1,16 @@
 
+// Step-1: Add event listener to the comment post button.
 document.getElementById("commentPostBtn").addEventListener('click', function() {
+
+    // Step-2: Get new comment value from comment input box.
     const commentInputValue = document.getElementById("commentInputBox").value;
+
+    // Step-3: Add paragraph tag to the comment display section.
     const newPeragraphTag = document.createElement('p');
-    newPeragraphTag.innerHTML = commentInputValue;
+    newPeragraphTag.innerText = commentInputValue;
     const commentsDisplaySection = document.getElementById("commentsDisplaySection");
     commentsDisplaySection.appendChild(newPeragraphTag);
+
+    // Step-4: Reset comment input box as empty.
     document.getElementById("commentInputBox").value = '';
 });
