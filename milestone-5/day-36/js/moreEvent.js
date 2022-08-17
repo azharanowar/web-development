9b5906f63b68420085ccfda3717508dcb45cb1ff
azharanowar <azharanowar@gmail.com>
2022-08-17 19:16:32 +0600

    Mouse events practiced

--- milestone-5/day-36/js/moreEvent.js
+++ milestone-5/day-36/js/moreEvent.js
@@ -22,6 +22,26 @@
 //     console.log("Mouse Move Event Triggered!")
 // });
 
-document.getElementById("inputField").addEventListener('mouseenter', function() {
-    console.log("Mouse Enter Event Triggered!")
+// document.getElementById("inputField").addEventListener('mouseenter', function() {
+//     console.log("Mouse Enter Event Triggered!")
+// });
+
+document.getElementById("inputField").addEventListener('mousedown', function() {
+    console.log("Mouse Down Event Triggered!")
+});
+
+document.getElementById("inputField").addEventListener('mouseleave', function() {
+    console.log("Mouse Leave Event Triggered!")
+});
+
+document.getElementById("inputField").addEventListener('mouseout', function() {
+    console.log("Mouse Out Event Triggered!")
+});
+
+document.getElementById("inputField").addEventListener('mouseover', function() {
+    console.log("Mouse Over Event Triggered!")
+});
+
+document.getElementById("inputField").addEventListener('mouseup', function() {
+    console.log("Mouse Up Event Triggered!")
 });
