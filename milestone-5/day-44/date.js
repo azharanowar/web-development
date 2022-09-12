9b48b887ad849a67919bcd333be0ca3b3b9ab906
azharanowar <azharanowar@gmail.com>
2022-09-12 13:38:05 +0600

    Javascript comparing two dates which one is bigger

--- milestone-5/day-44/date.js
+++ milestone-5/day-44/date.js
@@ -16,25 +16,41 @@
 // console.log(+date1, +date2);
 // console.log(date1.getTime(), date2.getTime());
 
-const date1 = new Date('1971-03-15');
-const date2 = new Date('1971-03-15');
-console.log(+date1, +date2);
 
-if ( +date1 === +date2 ) {
-    console.log("Both date are same!!!");
-} else {
-    console.log("Both date are not same!!!");
-} 
-// Now it working fine...
+// const date1 = new Date('1971-03-15');
+// const date2 = new Date('1971-03-15');
+// console.log(+date1, +date2);
 
-const dateOne = new Date('1971-03-15');
-const dateTwo = new Date('1971-03-15');
+// if ( +date1 === +date2 ) {
+//     console.log("Both date are same!!!");
+// } else {
+//     console.log("Both date are not same!!!");
+// } 
+// // Now it working fine...
+
+// const dateOne = new Date('1971-03-15');
+// const dateTwo = new Date('1971-03-15');
+// console.log(dateOne.getTime(), dateTwo.getTime());
+
+// if ( dateOne.getTime() === dateTwo.getTime() ) {
+//     console.log("Both date are same!!!");
+// } else {
+//     console.log("Both date are not same!!!");
+// }
+// // This way also working fine...
+
+
+
+// NOTE: JavaScript getTime() Comparison will start from this date: 1970-01-01 So when date will be 1970-01-01 getTime() output will be 0
+
+
+
+const dateOne = new Date('1971-10-15');
+const dateTwo = new Date('1971-01-25');
 console.log(dateOne.getTime(), dateTwo.getTime());
 
-if ( dateOne.getTime() === dateTwo.getTime() ) {
-    console.log("Both date are same!!!");
+if ( dateOne.getTime() > dateTwo.getTime() ) {
+    console.log("Date one is bigger than date two!!!");
 } else {
-    console.log("Both date are not same!!!");
+    console.log("Date two is bigger than date one!!!");
 }
-// This way also working fine...
-
