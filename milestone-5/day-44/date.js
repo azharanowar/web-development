79a6e47cb799a81eeff6be97d86677f3444cc606
azharanowar <azharanowar@gmail.com>
2022-09-12 13:17:22 +0600

    Javascript date comparison practice with using + operator before date and also with .getTime() method

--- milestone-5/day-44/date.js
+++ milestone-5/day-44/date.js
@@ -1,10 +1,40 @@
+// const date1 = new Date('1971-03-15');
+// const date2 = new Date('1971-03-15');
+// console.log(date1, date2);
+
+// if ( date1 === date2 ) {
+//     console.log("Both date are same!!!");
+// } else {
+//     console.log("Both date are not same!!!");
+// }
+// // JavaScript date comparison with date object not working because comparing of two values must be numbers.
+// // The reason behind that is: anything involving '==' or '===' should use the '+' prefix. So let's compare with this way:
+
+
+
+// // Let's solve this problem. Here is 2 way to fix that, both are same:
+// console.log(+date1, +date2);
+// console.log(date1.getTime(), date2.getTime());
+
 const date1 = new Date('1971-03-15');
 const date2 = new Date('1971-03-15');
-console.log(date1, date2);
+console.log(+date1, +date2);
 
-if ( date1 === date2 ) {
+if ( +date1 === +date2 ) {
+    console.log("Both date are same!!!");
+} else {
+    console.log("Both date are not same!!!");
+} 
+// Now it working fine...
+
+const dateOne = new Date('1971-03-15');
+const dateTwo = new Date('1971-03-15');
+console.log(dateOne.getTime(), dateTwo.getTime());
+
+if ( dateOne.getTime() === dateTwo.getTime() ) {
     console.log("Both date are same!!!");
 } else {
     console.log("Both date are not same!!!");
 }
-// The reason behind that is: anything involving '==' or '===' should use the '+' prefix. So let's compare with this way:
+// This way also working fine...
+
