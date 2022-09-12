74289da60c6f9ca844542435e15ab5c894a8f985
azharanowar <azharanowar@gmail.com>
2022-09-12 11:18:01 +0600

    JS interpreter and working process practiced

---
+++ milestone-5/day-44/try-catch-final.js
@@ -0,0 +1,14 @@
+/*
+
+console.log("Starting from here...");
+
+const num1 = 50;
+console.log(num1);
+// From here everything working fine.
+
+console.log(num2);      // Here will be ReferenceError because we called num2 before initialization.
+const num2 = 20;
+
+console.log("Hello World!");    // For our previous error that occurred, The JavaScript interpreter will stop working that's why this line will not be run.
+
+*/
