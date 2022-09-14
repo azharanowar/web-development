d41b0953cc2aa12fe1a3a33e30a57f57a98c8ddf
azharanowar <azharanowar@gmail.com>
2022-09-14 17:53:35 +0600

    Add new friend function and align center function added

--- milestone-6/day-45/recap-js-dom/js/script.js
+++ milestone-6/day-45/recap-js-dom/js/script.js
@@ -1,11 +1,21 @@
-document.getElementById("addNewFriend").addEventListener('click', function() {
-    alert("hello")
+document.getElementById("addNewFriendBtn").addEventListener('click', function() {
+    const newFriend = document.createElement("div");
+    newFriend.setAttribute("class", "friend");
+    newFriend.innerHTML = `
+    <h2>Friends Name - </h2>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Libero, ea. Facere consectetur quae praesentium eum itaque.
+    </p>`;
+    document.getElementById("listOfFriends").appendChild(newFriend)
+    
 });
 
-document.getElementById("appyBackground").addEventListener('click', function() {
-    const friends = document.getElementsByClassName("friends");
-    console.log(friends)
+document.getElementById("appyBackgroundBtn").addEventListener('click', function() {
+    const friends = document.getElementsByClassName("friend");
     for ( const friend of friends ) {
         friend.style.backgroundColor = "coral";
     }
+});
+
+document.getElementById("alignCenterBtn").addEventListener('click', function() {
+    document.getElementById("listOfFriends").style.textAlign = "center";
 });
