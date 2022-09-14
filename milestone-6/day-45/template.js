d9781cef259bd232022ff5b49963687a306dddda
azharanowar <azharanowar@gmail.com>
2022-09-14 20:47:54 +0600

    Javascript Template using multiline code with backtick/backquote

--- milestone-6/day-45/template.js
+++ milestone-6/day-45/template.js
@@ -1,9 +1,20 @@
 // There is a way to use multiline code with js but not recommended way.
 const single = "Hello Bangladesh!!!";
 
-const multiline = "Hello Bangladesh!!!\n" + 
-"Hello World!!!\n" + 
-"Hello Turkey!!!\n" + 
-"Hello Finland!!!";
+const multiline = "Hello Bangladesh!!!\n" +
+    "Hello World!!!\n" +
+    "Hello Turkey!!!\n" +
+    "Hello Finland!!!\n";
 
 console.log(multiline);
+
+
+// Standard way to write multiline code with js template with backtick/backquote:
+
+const multilineCode = `
+Hello World!!!
+Hello Turkey!!!
+Hello Finland!!!
+Hello Bangladesh!!!
+`;
+console.log(multilineCode);
