cf8e85fe99f04e9887164e6e0a6063ad7a2ea54d
azharanowar <azharanowar@gmail.com>
2022-09-14 20:40:50 +0600

    Multiline code in JS not recommended way

--- milestone-6/day-45/template.js
+++ milestone-6/day-45/template.js
@@ -1,10 +1,9 @@
+// There is a way to use multiline code with js but not recommended way.
 const single = "Hello Bangladesh!!!";
-
 
 const multiline = "Hello Bangladesh!!!\n" + 
 "Hello World!!!\n" + 
 "Hello Turkey!!!\n" + 
 "Hello Finland!!!";
 
-
-console.log(multiline)
+console.log(multiline);
