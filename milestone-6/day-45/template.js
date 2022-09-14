72167368afe50effd26b2727f7987354ceacc0bd
azharanowar <azharanowar@gmail.com>
2022-09-14 21:02:34 +0600

    Using javascript template benifit and backtick practiced

--- milestone-6/day-45/template.js
+++ milestone-6/day-45/template.js
@@ -18,3 +18,22 @@ Hello Finland!!!
 Hello Bangladesh!!!
 `;
 console.log(multilineCode);
+
+
+
+
+// Advanced benifit of javascript template to make string dynamic:
+
+// This is very complex way to make a string dynamic with normal single quote ( ' ' ) and double quote ( " " ). Example: 
+const a = 10;
+const b = 20;
+const c = a+b;
+const result = "a = " + a + "; b = " + b + "; a + b = " + c + ";";
+console.log(result);
+
+
+//  We can do that as simple with javascript template and backtick:
+const x = 10;
+const y = 30;
+const result1 = `x = ${x}; y = ${y}; x + y = ${x+y};`;
+console.log(result1);
