0df11c4c12145f857533053a2994b1c4cb2103ef
azharanowar <azharanowar@gmail.com>
2022-09-14 20:38:52 +0600

    Multiline code in javascript with + and \n

---
+++ milestone-6/day-45/template.js
@@ -0,0 +1,10 @@
+const single = "Hello Bangladesh!!!";
+
+
+const multiline = "Hello Bangladesh!!!\n" + 
+"Hello World!!!\n" + 
+"Hello Turkey!!!\n" + 
+"Hello Finland!!!";
+
+
+console.log(multiline)
