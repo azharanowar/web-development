354b450ed5a21e1da87704cff985763e8e3d3569
azharanowar <azharanowar@gmail.com>
2022-09-15 22:27:47 +0600

    JavaScript spread syntax practiced

---
+++ milestone-6/day-45/spread.js
@@ -0,0 +1,5 @@
+// Spread syntax (...) is provide direct value from an iterable/repeatable object (array, string)
+
+const numbers = [10, 20, 30, 40, 50, 20, 500, 44];
+console.log(numbers);
+console.log(...numbers);  // spread syntax
