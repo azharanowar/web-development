b2b116fd8d762bd3c9c7d502759094a9c3e0fd0a
azharanowar <azharanowar@gmail.com>
2022-09-16 19:21:04 +0600

    Destructuring practice

---
+++ milestone-6/day-45/destructuring.js
@@ -0,0 +1,13 @@
+const studentInfo = {
+    name: "Anowar Hosen",
+    roll: 181040,
+    registration: 1500980692,
+    depertment: "Computer"
+}
+
+
+const {name, roll, registration} = studentInfo;
+console.log(name);
+console.log(roll);
+console.log(registration);
+// console.log(depertment);
