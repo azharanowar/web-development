ba6403d3662f114de04c07c6d4a90a0b74027fb5
azharanowar <azharanowar@gmail.com>
2022-09-17 22:37:40 +0600

    File structure changed

--- milestone-6/day-45/practice-problem/problem-1.js
+++ milestone-6/day-45/practice-problem/problem-1.js
@@ -3,3 +3,10 @@
 const mul = (numberOne, numberTwo, numberThree) => numberOne * numberTwo * numberThree;
 
 console.log(mul(2,2,2));
+
+
+// Write the following sentence in three lines and print the result: I am a web developer. I love to code. I love to eat biryani.
+const messeges = `I'm a web developer.
+I love code.
+I love to eat biriyani.`
+console.log(messeges);
--- milestone-6/day-45/practice-problem/problem-2.js
+++
@@ -1,5 +0,0 @@
-// Write the following sentence in three lines and print the result: I am a web developer. I love to code. I love to eat biryani.
-const messeges = `I'm a web developer.
-I love code.
-I love to eat biriyani.`
-console.log(messeges);
