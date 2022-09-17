d0920fe8c22aad85cfced601f26c0adccb38822f
azharanowar <azharanowar@gmail.com>
2022-09-17 22:26:59 +0600

    Array desructuring practiced

--- milestone-6/day-45/destructuring.js
+++ milestone-6/day-45/destructuring.js
@@ -11,3 +11,12 @@ console.log(name);
 console.log(roll);
 console.log(registration);
 // console.log(depertment);     // As we didn't declare depertment with the object as we did with name, roll, and registration. 
+
+
+
+// Array destructuring:
+
+const studentsName = ["Anowar", "Arafat", "Shakhawat", "Shorip"];
+const [student1, student2, student3] = studentsName;
+
+console.log(student3);
