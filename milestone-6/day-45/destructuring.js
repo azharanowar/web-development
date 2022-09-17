b68227e58a75b88e118f2de1e376271299db23be
azharanowar <azharanowar@gmail.com>
2022-09-17 12:37:15 +0600

    Destructuring comment added

--- milestone-6/day-45/destructuring.js
+++ milestone-6/day-45/destructuring.js
@@ -10,4 +10,4 @@ const {name, roll, registration} = studentInfo;
 console.log(name);
 console.log(roll);
 console.log(registration);
-// console.log(depertment);
+// console.log(depertment);     // As we didn't declare depertment with the object as we did with name, roll, and registration. 
