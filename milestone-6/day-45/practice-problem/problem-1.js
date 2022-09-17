43eebf26bc20cf77b54db57398226a4a77cf3561
azharanowar <azharanowar@gmail.com>
2022-09-17 22:31:42 +0600

    Practice problem one solved

---
+++ milestone-6/day-45/practice-problem/problem-1.js
@@ -0,0 +1,5 @@
+// Write an arrow function that will take 3 parameters, will multiply the parameters and will return the result.
+
+const mul = (numberOne, numberTwo, numberThree) => numberOne * numberTwo * numberThree;
+
+console.log(mul(2,2,2));
