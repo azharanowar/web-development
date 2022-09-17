bd883ee060485a76100192d3ed5c227ec8b18831
azharanowar <azharanowar@gmail.com>
2022-09-17 22:40:30 +0600

    Practice problem one part completed

--- milestone-6/day-45/practice-problem/problem-1.js
+++ milestone-6/day-45/practice-problem/problem-1.js
@@ -10,3 +10,9 @@ const messeges = `I'm a web developer.
 I love code.
 I love to eat biriyani.`
 console.log(messeges);
+
+
+// Write an arrow function that will take 2 parameters: One parameter will come from you and the other parameter will be a default parameter. Add these two parameters and return the result.
+
+const sum = (numOne, numTwo = 20) => numOne + numTwo;
+console.log(sum(30));
