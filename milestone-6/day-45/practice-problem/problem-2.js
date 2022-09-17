ddd037d77709a995f16392d7c49159a2e4f108ef
azharanowar <azharanowar@gmail.com>
2022-09-17 22:36:42 +0600

    Practice problem-2 solved

---
+++ milestone-6/day-45/practice-problem/problem-2.js
@@ -0,0 +1,5 @@
+// Write the following sentence in three lines and print the result: I am a web developer. I love to code. I love to eat biryani.
+const messeges = `I'm a web developer.
+I love code.
+I love to eat biriyani.`
+console.log(messeges);
