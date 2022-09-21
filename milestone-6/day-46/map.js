b7076765ee772175661e738c3c8ebf63085d4f65
azharanowar <azharanowar@gmail.com>
2022-09-21 17:40:26 +0600

    JavaScript map practiced

--- milestone-6/day-46/map.js
+++ milestone-6/day-46/map.js
@@ -1,20 +1,22 @@
-function doubleNumbers(numbers) {
+const doubleIt = number => number * 2;
+
+function getDouble(numbers) {
     const newNumbers = [];
     for (const number of numbers) {
-        const doubleNumber = doubleItNew(number);
+        const doubleNumber = doubleIt(number);
         newNumbers.push(doubleNumber);
     }
     return newNumbers;
 }
 
 
-function doubleItOld(number) {
-    return number * 2;
-}
-
-
-const doubleItNew = number => number * 2;
-
-
 const numbers = [4, 12, 8, 3, 3, 2, 4, 5, 6];
-console.log(doubleNumbers(numbers));
+
+
+console.log(getDouble(numbers));
+
+console.log(numbers.map(doubleIt));
+console.log(numbers.map(double => double * 2));
+
+
+console.log([4, 12, 8, 3, 3, 2, 4, 5, 6].map(number => number * 2))
