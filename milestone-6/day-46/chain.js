8e86779ee0a891798753aa2a1cf73696c724b5d7
azharanowar <azharanowar@gmail.com>
2022-09-21 15:12:45 +0600

    JavaScript objects in array elements

---
+++ milestone-6/day-46/chain.js
@@ -0,0 +1,34 @@
+const studentsInfo = [
+    {
+        name: "Azhar Anowar",
+        roll: 181040,
+        registration: 1500980692,
+        depertment: "Computer Science and Technology",
+        shift: "2nd",
+        group: "B"
+    },
+    {
+        name: "Fozle Arafat",
+        roll: 169756,
+        registration: 1500000000,
+        depertment: "Computer Science and Technology",
+        shift: "2nd",
+        group: "A"
+    },
+    {
+        name: "Ariful Islam Nihad",
+        roll: 169748,
+        registration: 1500980000,
+        depertment: "Computer Science and Technology",
+        shift: "2nd",
+        group: "A"
+    }
+];
+
+
+console.log(studentsInfo[0].name);
+console.log(studentsInfo[0].roll);
+console.log(studentsInfo[0].registration);
+console.log(studentsInfo[0].depertment);
+console.log(studentsInfo[0].shift);
+console.log(studentsInfo[0].group);
