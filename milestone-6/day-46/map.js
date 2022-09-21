677e744cad5ce4b5cf4006a1956152d1780e1089
azharanowar <azharanowar@gmail.com>
2022-09-21 16:34:56 +0600

    Same work making a number double with javascript regular and arrow function

--- milestone-6/day-46/map.js
+++ milestone-6/day-46/map.js
@@ -1,4 +1,4 @@
-function doubleItOld(numbers) {
+function doubleNumbers(numbers) {
     const newNumbers = [];
     for (const number of numbers) {
         newNumbers.push(number * 2);
@@ -7,4 +7,15 @@ function doubleItOld(numbers) {
 }
 
 const numbers = [4, 12, 8, 3, 3, 2, 4, 5, 6];
-console.log(doubleItOld(numbers));
+console.log(doubleNumbers(numbers));
+
+
+
+function doubleOld(number) {
+    return number * 2;
+}
+console.log(doubleOld(10));
+
+
+const doubleNew = (number) => number * 2;
+console.log(doubleNew(10));
