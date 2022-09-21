89f392b384c5c994426183cc36bb581d0349b046
azharanowar <azharanowar@gmail.com>
2022-09-21 21:41:08 +0600

    Another JS filter practice

--- milestone-6/day-46/filter.js
+++ milestone-6/day-46/filter.js
@@ -1,4 +1,8 @@
-const numbers = [10, 30, 20, 12, 32, 40, 2, 30, 12, 22, 32, 4, 0, 13, 47];
+const numbers = [10, 30, 20, 13, 32, 40, 7, 30, 12, 22, 32, 4, 0, 13, 47];
 
 const greatherNumbers = numbers.filter(number => number > 20);
 console.log(greatherNumbers);
+
+
+const evenNumbers = numbers.filter(number => number % 2 === 0);
+console.log(evenNumbers)
