1948274b273855f2955960dcf10e2a7d36041e86
azharanowar <azharanowar@gmail.com>
2022-09-21 15:42:12 +0600

    Optional Chaining/Nested object optional chain practiced

--- milestone-6/day-46/chain.js
+++ milestone-6/day-46/chain.js
@@ -5,7 +5,16 @@ const studentsInfo = [
         registration: 1500980692,
         depertment: "Computer Science and Technology",
         shift: "2nd",
-        group: "B"
+        group: "B",
+        address: {
+            street: {
+                firstLine: "Kalim Uddin Hazi Bari, MohammadPur",
+                secondLine: "Senbag, Noakhali."
+            },
+            postOffice: 3861,
+            district: "Noakhali",
+            country: "Bangladesh"
+        }
     },
     {
         name: "Fozle Arafat",
@@ -26,9 +35,30 @@ const studentsInfo = [
 ];
 
 
+// Array 0 index has value of address so here everything will work fine.
 console.log(studentsInfo[0].name);
 console.log(studentsInfo[0].roll);
 console.log(studentsInfo[0].registration);
 console.log(studentsInfo[0].depertment);
 console.log(studentsInfo[0].shift);
 console.log(studentsInfo[0].group);
+console.log(studentsInfo[0].address.street.firstLine);
+console.log(studentsInfo[0].address.street.secondLine);
+console.log(studentsInfo[0].address.postOffice);
+console.log(studentsInfo[0].address.district);
+console.log(studentsInfo[0].address.country);
+
+
+
+// But array 1/2 index has no key/attribute of address. So we will get TypeError when we will try to read a attribute that accually not exist:
+console.log(studentsInfo[1].name);
+console.log(studentsInfo[1].roll);
+console.log(studentsInfo[1].registration);
+console.log(studentsInfo[1].depertment);
+console.log(studentsInfo[1].shift);
+console.log(studentsInfo[1].group);
+console.log(studentsInfo[1].address?.street.firstLine);
+console.log(studentsInfo[1].address?.street.secondLine);
+console.log(studentsInfo[1].address?.postOffice);
+console.log(studentsInfo[1].address?.district);
+console.log(studentsInfo[1].address?.country);
