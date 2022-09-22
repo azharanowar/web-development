7749e3bbd1875e296e12219ed57ad1a107f941e4
azharanowar <azharanowar@gmail.com>
2022-09-22 16:47:15 +0600

    JavaScript Object to Convert JSON String

--- milestone-6/day-48/json.js
+++ milestone-6/day-48/json.js
@@ -1,3 +1,33 @@
 // JSON - JavaScript Object Notation
 
 // JSON is used to store or transmit data from a server to client application. 
+
+
+const userInfo = {
+    id: 1,
+    name: "Azhar Anowar",
+    username: "azharanowar",
+    website: 'www.azharanowar.com',
+};
+
+console.log(userInfo);  // JavaScript Object Formate
+
+const userStringified = JSON.stringify(userInfo);   // Convert JavaScript Object to JSON String.
+
+console.log(userStringified);
+
+
+/* 
+JavaScript Object: 
+{
+  id: 1,
+  name: 'Azhar Anowar',
+  username: 'azharanowar',
+  website: 'www.azharanowar.com'
+}
+
+JSON String: 
+{"id":1,"name":"Azhar Anowar","username":"azharanowar","website":"www.azharanowar.com"}
+
+*/
+
