e8085d3535021c87a45bbcf2994160eb964fb7c9
azharanowar <azharanowar@gmail.com>
2022-09-22 18:28:48 +0600

    Another way to call JSON Placeholder

--- milestone-6/day-48/js/fetch.js
+++ milestone-6/day-48/js/fetch.js
@@ -1,4 +1,12 @@
-
+/* 
 fetch('https://jsonplaceholder.typicode.com/todos/1')
   .then(response => response.json())
   .then(json => console.log(json))
+*/
+
+
+const url = 'https://jsonplaceholder.typicode.com/todos/1';
+fetch(url)
+  .then(response => response.json())
+  .then(json => console.log(json))
+
