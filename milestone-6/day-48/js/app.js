4d9166949247b31d8a526616985f858de405a61d
azharanowar <azharanowar@gmail.com>
2022-09-22 20:18:25 +0600

    loadUsers from a API/JSON Placeholder and get all users name

--- milestone-6/day-48/js/app.js
+++ milestone-6/day-48/js/app.js
@@ -3,3 +3,15 @@ function loadData(url) {
       .then(response => response.json())
       .then(data => console.log(data))
 }
+
+
+const loadUsers = () => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(data => displayUsers(data))
+}
+
+const displayUsers = data => {
+    const usersName = data.map(user => user.name);
+    console.log(usersName);
+}
