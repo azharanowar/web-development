8edb6b7c9b67f77a4c11aaef05b49196764f9e95
azharanowar <azharanowar@gmail.com>
2022-09-22 18:33:24 +0600

    dataLoad function for API/URL data load

--- milestone-6/day-48/js/fetch.js
+++ milestone-6/day-48/js/fetch.js
@@ -12,12 +12,22 @@ fetch(url)
 */
 
 
+function loadData(url) {
+    fetch(url)
+      .then(response => response.json())
+      .then(data => console.log(data))
+}
+
+loadData('https://jsonplaceholder.typicode.com/todos/1');
 
 
 
 
-// Load all posts from a API/URL:
+
+/* 
+// Load all posts from an API/URL:
 fetch('https://jsonplaceholder.typicode.com/posts')
   .then(response => response.json())
   .then(json => console.log(json));
 
+*/
