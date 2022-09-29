c5d55775474d9713a56f12f592f6c4a2aba9c29d
azharanowar <azharanowar@gmail.com>
2022-09-29 22:16:57 +0600

    JavaScript object practice

---
+++ milestone-6/day-52/differents-way-to-create-object.js
@@ -0,0 +1,14 @@
+const student = {
+    name: "Azhar Anowar",
+    roll: 181040,
+    reg: 1500980692,
+    semester: '8th',
+    shift: '2nd',
+    group: 'B',
+    subjectsList: ["Mathematics", "Physics", "Chemistry", "English", "Microprocessor", "Programming", "Data Structure", "Database"]
+}
+
+console.log(student);
+
+const studentKeys = Object.keys(student);
+console.log(studentKeys)
