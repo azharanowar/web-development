ab06d10158500992f5b30b2f2e2ca0747dd74d69
azharanowar <azharanowar@gmail.com>
2022-09-30 22:55:19 +0600

    We can't use for of in a object. Because for of isn't iterable like array is.

---
+++ milestone-6/day-52/object-value-access.js
@@ -0,0 +1,21 @@
+const student = {
+    name: "Azhar Anowar",
+    roll: 181040,
+    reg: 1500980692,
+    semester: '8th',
+    shift: '2nd',
+    group: 'B',
+    subjectsList: ["Mathematics", "Physics", "Chemistry", "English", "Microprocessor", "Programming", "Data Structure", "Database"],
+    exam: function() {
+        console.log(this.name + ' is sitting the final year exam of diploma ' + this.semester + ' semester.');
+    }
+};
+
+console.log(student);
+
+console.log(student.roll);
+
+// We can't use for of in a object. Because for of isn't iterable like array is.
+// for(const item of student) {
+//     console.log(item);
+// }
