18612bb9d4a165d4d9426ace98d08227625e87f7
azharanowar <azharanowar@gmail.com>
2022-09-30 21:32:39 +0600

    Differents way to create object with JavaScript

--- milestone-6/day-52/differents-way-to-create-object.js
+++ milestone-6/day-52/differents-way-to-create-object.js
@@ -1,3 +1,4 @@
+// Create object using object literals: 
 const student = {
     name: "Azhar Anowar",
     roll: 181040,
@@ -5,10 +6,30 @@ const student = {
     semester: '8th',
     shift: '2nd',
     group: 'B',
-    subjectsList: ["Mathematics", "Physics", "Chemistry", "English", "Microprocessor", "Programming", "Data Structure", "Database"]
+    subjectsList: ["Mathematics", "Physics", "Chemistry", "English", "Microprocessor", "Programming", "Data Structure", "Database"],
+    exam: function() {
+        console.log(this.name + ' is sitting the final year exam of diploma ' + this.semester + ' semester.');
+    }
 }
 
-console.log(student);
+// console.log(student);
+student.exam();
 
-const studentKeys = Object.keys(student);
-console.log(studentKeys)
+// Create object using new keyword with Object constructor:
+const institute = new Object;
+console.log(institute);
+
+institute.name = "Feni Polytechnic Institute";
+institute.location = "Feni Pourshava, Feni";
+institute.code = "00000";
+
+console.log(institute);
+
+
+// By Object.create() method:
+
+const person = Object.create(student);  // Here we will get all properties and methods from student.
+console.log(person);
+person.fathersName = "Jaynal Abedin";
+console.log(person.fathersName);
+console.log(person.name);   // Here with person object we don't have any property call name but it still getting azhar anowar. That's mean we made connection with person object to student object.
